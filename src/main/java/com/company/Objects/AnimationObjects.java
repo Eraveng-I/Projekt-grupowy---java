02ed9a8f8121055af61183554962c4f07bc8f22a
PeterThere <72750867+PeterThere@users.noreply.github.com>
2022-01-24 02:02:09 +0100

    Przesunięcie zasobów do folderu resources

--- src/main/java/com/company/Objects/AnimationObjects.java
+++ src/main/java/com/company/Objects/AnimationObjects.java
@@ -20,15 +20,15 @@ public class AnimationObjects {
     public AnimationObjects(AnimationPanel panel){this.panel = panel;}
 
     public void prepareObjects() {
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag0.png").getImage(),Color.cyan);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag1.png").getImage(),Color.GREEN);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag2.png").getImage(),Color.GREEN);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag3.png").getImage(),Color.GREEN);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag4.png").getImage(),Color.RED);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag5.png").getImage(),Color.GREEN);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag6.png").getImage(),Color.GREEN);
-        Flag.setUpFlag(panel, objects, new ImageIcon("flag7.png").getImage(),Color.GREEN);
-        ball = GoldenBall.setUpBall(panel, Color.decode("#D8C841"), new ImageIcon("ballondor.png").getImage());
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag0.png").getImage(),Color.cyan);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag1.png").getImage(),Color.GREEN);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag2.png").getImage(),Color.GREEN);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag3.png").getImage(),Color.GREEN);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag4.png").getImage(),Color.RED);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag5.png").getImage(),Color.GREEN);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag6.png").getImage(),Color.GREEN);
+        Flag.setUpFlag(panel, objects, new ImageIcon("src/main/resources/flag7.png").getImage(),Color.GREEN);
+        ball = GoldenBall.setUpBall(panel, Color.decode("#D8C841"), new ImageIcon("src/main/resources/ballondor.png").getImage());
         executorService = Executors.newFixedThreadPool(objects.size());
     }
     public void updateParallel()
